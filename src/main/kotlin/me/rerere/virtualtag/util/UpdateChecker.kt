6f9f47e606841b5f4b9ee06cc011e44b82363487
Loyisa <30242618+Loyisa@users.noreply.github.com>
2025-12-14 23:59:30 +0800

    Replace OkHttp with HttpURLConnection

--- src/main/kotlin/me/rerere/virtualtag/util/UpdateChecker.kt
+++ src/main/kotlin/me/rerere/virtualtag/util/UpdateChecker.kt
@@ -1,26 +1,18 @@
 package me.rerere.virtualtag.util
 
 import me.rerere.virtualtag.virtualTag
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import java.util.concurrent.TimeUnit
+import java.net.HttpURLConnection
+import java.net.URL
 
 object UpdateChecker {
     fun getLatestVersion(): String = try {
-        val httpClient = OkHttpClient.Builder()
-            .connectTimeout(10, TimeUnit.SECONDS)
-            .readTimeout(10, TimeUnit.SECONDS)
-            .writeTimeout(10, TimeUnit.SECONDS)
-            .build()
-        val request = Request.Builder()
-            .url("https://cdn.jsdelivr.net/gh/re-ovo/VirtualTag/build.gradle.kts")
-            .get()
-            .build()
-        val response = httpClient.newCall(request).execute()
-        val body = response.body!!.string()
-        body.substring(
-            body.indexOf("version = \"") + "version = \"".length,
-            body.indexOf(char = '"', startIndex = body.indexOf("version = \"") + "version = \"".length)
+        val url = URL("https://cdn.jsdelivr.net/gh/re-ovo/VirtualTag/build.gradle.kts")
+        val connection = url.openConnection() as HttpURLConnection
+        val inputStream = connection.inputStream
+        val responseBody = inputStream.bufferedReader().use { it.readText() }
+        responseBody.substring(
+            responseBody.indexOf("version = \"") + "version = \"".length,
+            responseBody.indexOf('"', responseBody.indexOf("version = \"") + "version = \"".length)
         ).trim()
     } catch (e: Exception) {
         e.printStackTrace()
