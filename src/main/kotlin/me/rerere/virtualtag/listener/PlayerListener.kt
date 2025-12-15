fe0538a7a5779c4d3b2aeef1940ecbbc076852f5
Loyisa <30242618+Loyisa@users.noreply.github.com>
2025-12-15 20:10:16 +0800

    Add async support for player join/quit tag updates

--- src/main/kotlin/me/rerere/virtualtag/listener/PlayerListener.kt
+++ src/main/kotlin/me/rerere/virtualtag/listener/PlayerListener.kt
@@ -1,5 +1,6 @@
 package me.rerere.virtualtag.listener
 
+import me.rerere.virtualtag.util.TaskUtil
 import me.rerere.virtualtag.virtualTag
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
@@ -11,13 +12,29 @@ class PlayerListener : Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     fun onJoin(event: PlayerJoinEvent) {
         val player = event.player
-        virtualTag().tagHandler.sendCurrentNameTags(player)
-        virtualTag().tagManager.updatePlayerTag(player)
+        val updateTask: () -> Unit = {
+            virtualTag().tagHandler.sendCurrentNameTags(player)
+            virtualTag().tagManager.updatePlayerTag(player)
+        }
+        if (virtualTag().configModule.mainConfig.asyncUpdate) {
+            TaskUtil.asyncTask(updateTask)
+        } else {
+            updateTask.invoke()
+        }
+
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
-    fun onQuit(event: PlayerQuitEvent){
-        virtualTag().tagManager.playerQuit(event.player)
-        virtualTag().tagHandler.removePlayerTag(event.player)
+    fun onQuit(event: PlayerQuitEvent) {
+        val player = event.player
+        val removeTask: () -> Unit = {
+            virtualTag().tagManager.playerQuit(player)
+            virtualTag().tagHandler.removePlayerTag(player)
+        }
+        if (virtualTag().configModule.mainConfig.asyncUpdate) {
+            TaskUtil.asyncTask(removeTask)
+        } else {
+            removeTask.invoke()
+        }
     }
 }
